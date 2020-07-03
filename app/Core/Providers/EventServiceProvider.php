a708974f6ca1c37ac0bce89322f2d29d70bcb33e
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-07-03 15:45:28 -0300

    registra o evento de aluno matriculado

--- app/Core/Providers/EventServiceProvider.php
+++ app/Core/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace Ddd\Arquitetura\Core\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Registered;
+use Ddd\Arquitetura\Dominios\Academico\Aluno\AlunoMatriculado;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Ddd\Arquitetura\Dominios\Academico\Matricula\EnviarEmailAlunoMatriculado;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        AlunoMatriculado::class => [
+            EnviarEmailAlunoMatriculado::class
+        ]
     ];
 
     /**
