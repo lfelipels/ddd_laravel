7eb80428ce657fd116082635bd3ea2e9d00518a8
Luis Felipe <luisfelipelimadasilva@gmail.com>
2020-07-04 14:55:48 -0300

    add email ao repositório de aluno

--- app/Dominios/Academico/Aluno/RepositorioDeAluno.php
+++ app/Dominios/Academico/Aluno/RepositorioDeAluno.php
@@ -3,9 +3,10 @@
 namespace Ddd\Arquitetura\Dominios\Academico\Aluno;
 
 use Ddd\Arquitetura\Dominios\Shared\Cpf;
+use Ddd\Arquitetura\Dominios\Shared\Email;
 
 interface RepositorioDeAluno
 {
-    public function adicionar(Cpf $cpf, string $nome): void;
+    public function adicionar(Cpf $cpf, string $nome, Email $email): void;
     public function localizarPorCpf(Cpf $cpf): ?Aluno;
 }
--- app/Dominios/Academico/Aluno/RepositorioDeAlunoComEloquent.php
+++ app/Dominios/Academico/Aluno/RepositorioDeAlunoComEloquent.php
@@ -5,14 +5,16 @@ namespace Ddd\Arquitetura\Dominios\Academico\Aluno;
 use Ddd\Arquitetura\Dominios\Academico\Aluno\Aluno;
 use Ddd\Arquitetura\Dominios\Academico\Aluno\RepositorioDeAluno;
 use Ddd\Arquitetura\Dominios\Shared\Cpf;
+use Ddd\Arquitetura\Dominios\Shared\Email;
 
 class RepositorioDeAlunoComEloquent implements RepositorioDeAluno
 {
-    public function adicionar(Cpf $cpf, string $nome): void
+    public function adicionar(Cpf $cpf, string $nome, Email $email): void
     {
         $aluno = new Aluno;
         $aluno->nome = $nome;
         $aluno->cpf = $cpf;
+        $aluno->email = $email;
         $aluno->save();
     }
 
